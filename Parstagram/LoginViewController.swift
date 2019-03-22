73a71e22f6537176abb95665970924d5ca74ba0b
Joyce <joycalation@gmail.com>
2019-03-21 21:57:35 -0400

    Added launch screen & styled buttons

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -11,13 +11,17 @@ import Parse
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        // Remove the method below for styling the button
+        self.styleButtons()
     }
     
     @IBAction func onSignUp(_ sender: Any) {
@@ -45,6 +49,27 @@ class LoginViewController: UIViewController {
                 print ("Error: \(error?.localizedDescription)")
             }
         }
+    }
+    
+    
+    fileprivate func styleButtons() {
+        signupButton.translatesAutoresizingMaskIntoConstraints = false
+        signupButton.layer.borderWidth = 1
+        signupButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        signupButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 9).isActive = true
+        signupButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+        signupButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        signupButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.layer.borderWidth = 1
+        
+       
+        loginButton.topAnchor.constraint(equalTo: self.signupButton.bottomAnchor, constant: 20).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        
     }
     
     /*
